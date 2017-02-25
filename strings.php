aa12238d307f8c1bfd5c09cd658fbf8a21230f69
MegaOS <megaspprt@gmail.com>
2017-02-25 13:00:52 +0800

    Create strings.php

---
+++ strings.php
@@ -0,0 +1,4 @@
+* explode // split strings to arrays of strings by comma
+* implode // To join back an array to a single string separated with commas
+* strlen // To measure the length of a string
+* substr // To cut a part of a string and return it as a new string
